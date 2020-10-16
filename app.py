eb599e4989ae4d66ffe6f5022b85f1a33e89a018
Tom Bell <tjbell@gmail.com>
2020-10-15 18:02:30 -0600

    refactor: item to product

--- app.py
+++ app.py
@@ -4,20 +4,20 @@ app = Flask(__name__)
 
 categories = {
     "collectables": {
-        "items": {
-            "card": {
-                "title": "Honus Wagner - 2009 Topps (PSA GEM MT 10)",
-                "img": "img/card.jpg",
-                "description": "2009 TOPPS T-206 HONUS WAGNER PSA GEM MT 10. Shipped with USPS First Class.",
-                "price": "$259,000.00",
-            },
-        },
+        "products": [{
+            "title": "Honus Wagner - 2009 Topps (PSA GEM MT 10)",
+            "img": "img/card.jpg",
+            "description": "2009 TOPPS T-206 HONUS WAGNER PSA GEM MT 10. Shipped with USPS First Class.",
+            "price": "$259,000.00",
+        },],
         "title": "Collectibles & Art",
         "subtitle": "Discover coins, stamps, comics, and more.",
+        "route": "collectables",
     },
     "electronics": {
-        "items": {},
+        "products": [{}],
         "title": "Electronics",
         "subtitle": "Discover computers, cameras, TVs, and more.",
+        "route": "electronics",
     },
 }
